0b43bb7b6a4b8f9ea6a09c7dd0372754f01537a3
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 18:33:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  all = true 
+  while i < collection.length 
+    if yield(array[i]) == true 
+      i += 1 
+    else 
+      all = false 
+      return false 
+    end
+  end 
 end
