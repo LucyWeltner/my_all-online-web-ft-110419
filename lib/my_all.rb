00cd23514da258a869f67ab7bc062b01b2240255
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 18:39:16 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,11 +4,11 @@ def my_all?(collection)
   i = 0
   all = true 
   while i < collection.length 
-    if yield(collection[i]) == true 
-      i += 1 
-    else 
+    if yield(collection[i]) == false
       all = false 
       return all 
+    else
+      i += 1 
     end
   end 
   all
